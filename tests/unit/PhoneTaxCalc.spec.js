a2ef2da69e053f4b5abe9406a60619b35b2a29b3
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-06-07 05:11:16 +0300

    Sabitler TaxCalc içinden kullanılır hale getirildi

--- tests/unit/PhoneTaxCalc.spec.js
+++ tests/unit/PhoneTaxCalc.spec.js
@@ -1,96 +1,97 @@
 import PhoneTaxCalc from "../../src/calculators/PhoneTaxCalc";
+import TaxCalc from "../../src/calculators/TaxCalc";
 /* eslint-disable jest/expect-expect */
 
 
 // While testing, all currencies is 0
 describe("PhoneTaxCalc", () => {
-	it(`Mode: "${PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_IMPORT}"`, () => {
+	it(`Mode: "${TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE}" / Registration: "${TaxCalc.REGISTRATION_IMPORT}"`, () => {
 		calculate([
 			{
 				price: 500,
 				expectedPrice: 305.12,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			},
 			{
 				price: 1500,
 				expectedPrice: 817.27,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			},
 			{
 				price: 5000,
 				expectedPrice: 2542.63,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			}
 		]);
 	});
 
-	it(`Mode: "${PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_IMPORT}"`, () => {
+	it(`Mode: "${TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_IMPORT}"`, () => {
 		calculate([
 			{
 				price: 500,
 				expectedPrice: 818.63,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			},
 			{
 				price: 1500,
 				expectedPrice: 2947.05,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			},
 			{
 				price: 5000,
 				expectedPrice: 9823.5,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_IMPORT
 			}
 		]);
 	});
 
-	it(`Mode: "${PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_PASSPORT}"`, () => {
+	it(`Mode: "${TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_PASSPORT}"`, () => {
 		calculate([
 			{
 				price: 500,
 				expectedPrice: -1338,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			},
 			{
 				price: 1500,
 				expectedPrice: -338,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			},
 			{
 				price: 5000,
 				expectedPrice: 3162,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			}
 		]);
 	});
 
-	it(`Mode: "${PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_PASSPORT}"`, () => {
+	it(`Mode: "${TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE}" / Registration: "${PhoneTaxCalc.REGISTRATION_PASSPORT}"`, () => {
 		calculate([
 			{
 				price: 500,
 				expectedPrice: 2338,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			},
 			{
 				price: 1500,
 				expectedPrice: 3338,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			},
 			{
 				price: 5000,
 				expectedPrice: 6838,
-				mode: PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
+				mode: TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE,
 				registration: PhoneTaxCalc.REGISTRATION_PASSPORT
 			}
 		]);
@@ -108,11 +109,11 @@ function calculate(scenarios) {
 		}).calculate();
 
 		switch (mode) {
-			case PhoneTaxCalc.MODE_CALCULATE_FROM_SALE_PRICE:
+			case TaxCalc.MODE_CALCULATE_FROM_SALE_PRICE:
 				expect(calculator.prices.basePrice).toBe(expectedPrice);
 				break;
 
-			case PhoneTaxCalc.MODE_CALCULATE_FROM_BASE_PRICE:
+			case TaxCalc.MODE_CALCULATE_FROM_BASE_PRICE:
 				expect(calculator.prices.salePrice).toBe(expectedPrice);
 				break;
 		}
