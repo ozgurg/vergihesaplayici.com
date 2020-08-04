c18b9fd8cbe8df1d00816cab665c1e2d91610882
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-08-04 05:50:05 +0300

    Gereksiz olan setTimeout kaldırıldı

--- src/modules/ConsoleTaxCalculator/ConsoleTaxCalculator.vue
+++ src/modules/ConsoleTaxCalculator/ConsoleTaxCalculator.vue
@@ -143,17 +143,15 @@ export default {
 		const vm = this;
 
 		// Set data from query with validate them
-		setTimeout(() => {
-			if (vm.$route.query) {
-				if (vm.$route.query.price) {
-					vm.form.price = parseFloat(vm.$route.query.price);
-				}
+		if (vm.$route.query) {
+		  if (vm.$route.query.price) {
+				vm.form.price = parseFloat(vm.$route.query.price);
+		  }
 
-				if (vm.$route.query.currency && vm.$store.getters.getCurrencies.includes(vm.$route.query.currency)) {
-					vm.form.currency = vm.$route.query.currency;
-				}
-			}
-		}, 200);
+		  if (vm.$route.query.currency && vm.$store.getters.getCurrencies.includes(vm.$route.query.currency)) {
+				vm.form.currency = vm.$route.query.currency;
+		  }
+		}
 	},
 	methods: {
 		calculate: async function() {
