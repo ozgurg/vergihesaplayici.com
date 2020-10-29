3cc870cc52f7f1fc3ee1ec3488d40b7d0c2d4332
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-10-29 06:32:40 +0300

    Added Babel configuration file

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		test: {
+			presets: [
+				[
+					"@babel/preset-env",
+					{
+						targets: {
+							node: "current"
+						}
+					}
+				]
+			],
+			plugins: [
+				"@babel/plugin-proposal-class-properties"
+			]
+		}
+	}
+};
