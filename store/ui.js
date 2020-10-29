fe788504f995dda220b4cdd6e71620861d037cf8
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-10-29 06:29:12 +0300

    Added UI module to the store

---
+++ store/ui.js
@@ -0,0 +1,23 @@
+import { make } from "vuex-pathify";
+
+export const state = () => ({
+	drawerState: null,
+	isCenteredContent: false,
+	toolbarTitle: process.env.npm_package_name,
+	snackbar: {
+		timeout: 1000,
+		isVisible: false,
+		text: null
+	}
+});
+
+export const mutations = make.mutations(state);
+
+export const actions = make.actions(state);
+
+export default {
+	namespaced: true,
+	state,
+	mutations,
+	actions
+};
