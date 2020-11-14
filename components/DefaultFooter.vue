2ee3a08d1ac6b6f6ca01c52ee123fbad490f9181
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2020-11-15 02:36:20 +0300

    Updated footer

--- components/DefaultFooter.vue
+++ components/DefaultFooter.vue
@@ -1,16 +1,13 @@
 <template>
 	<v-footer v-bind="$attrs"
+			  class="d-flex justify-center text-center pa-2"
 			  padless="">
-		<v-row align="center"
-			   justify="center">
-			<v-col class="text-center"
-				   cols="12">
-				<p>vergihesaplayici.com,
-					<a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından açık
-					kaynaklı olarak geliştirilmektedir.</p>
-				<Ghbtns />
-			</v-col>
-		</v-row>
+		<div class="caption mb-3">vergihesaplayici.com,
+			<a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a>
+			tarafından açık
+			kaynaklı olarak geliştirilmektedir
+		</div>
+		<Ghbtns />
 	</v-footer>
 </template>
 
