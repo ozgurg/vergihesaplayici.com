826411d2bba0a7383fbc72411386203325b1ac3c
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-01-27 21:39:49 +0300

    Added <br /> to CustomsInfoAlert

--- components/calculators/CustomsInfoAlert.vue
+++ components/calculators/CustomsInfoAlert.vue
@@ -3,8 +3,10 @@
 			 dense=""
 			 text=""
 			 border="left"
-			 type="warning">Ürünlerin ülkeye giriş fiyatını bilmediğimiz için gösterilen fiyatlar tahminidir. Gerçek
-		satış fiyatı farklı olabilir ve muhtemelen daha az olacaktır.
+			 type="warning">
+		Ürünlerin ülkeye giriş fiyatını bilmediğimiz için gösterilen fiyatlar tahminidir.
+		<br />
+		Gerçek satış fiyatı farklı olabilir ve muhtemelen daha az olacaktır.
 	</v-alert>
 </template>
 
