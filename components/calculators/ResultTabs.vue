32b5bd499ee586d7aa87ff889024428bd1e43df4
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-01-29 00:46:56 +0300

    When calculated, the window scrolls to tabs (Closes #131)

--- components/calculators/ResultTabs.vue
+++ components/calculators/ResultTabs.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<HorizontalForm class="mb-4">
+	<div ref="resultTab">
+		<HorizontalForm class="result-tab mb-4">
 			<v-tabs :value="value"
 					@change="$emit('input', $event)"
 					background-color="transparent"
@@ -40,6 +40,29 @@ export default {
 			type: Boolean,
 			default: false
 		}
+	},
+	watch: {
+		showResults() {
+			const vm = this;
+
+			if (vm.showResults) {
+				setTimeout(() => {
+					vm.$refs["resultTab"].scrollIntoView(true);
+				}, 0);
+			}
+		}
 	}
 };
 </script>
+
+<style lang="scss" scoped="">
+.result-tab {
+	position: sticky;
+	background: #121212;
+	z-index: 100;
+	top: 56px;
+	@media(min-width: 960px) {
+		top: 64px
+	}
+}
+</style>
