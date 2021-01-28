a4f74b6112ee13b30f705ed57c6c6731b4a954a8
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-01-29 00:16:11 +0300

    Added dots to calculator descriptions

--- data/calculators.js
+++ data/calculators.js
@@ -5,7 +5,7 @@ calculators["ConsoleTaxCalculator"] = {
 	"shortTitle": "Konsol Vergisi",
 	"title": "Konsol Vergisi Hesaplayıcı",
 	"url": "/hesaplayicilar/konsol-vergisi-hesaplayici",
-	"description": "Sadece eğlenmek istediğinizde ödemeniz gereken vergileri hesaplayın"
+	"description": "Sadece eğlenmek istediğinizde ödemeniz gereken vergileri hesaplayın."
 };
 
 calculators["PhoneTaxCalculator"] = {
@@ -13,7 +13,7 @@ calculators["PhoneTaxCalculator"] = {
 	"shortTitle": "Telefon Vergisi",
 	"title": "Telefon Vergisi Hesaplayıcı",
 	"url": "/hesaplayicilar/telefon-vergisi-hesaplayici",
-	"description": "Satın aldığınız bir telefonun satış fiyatına etki eden vergileri hesaplayın"
+	"description": "Satın aldığınız bir telefonun satış fiyatına etki eden vergileri hesaplayın."
 };
 
 module.exports = calculators;
