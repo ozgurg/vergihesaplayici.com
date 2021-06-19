4da337d0e457014232914df104e794a1ecb4b87a
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-06-20 01:28:58 +0300

    OsitaIheme is now a component

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,7 @@
         <InnerContainer>
             <p class="mb-4">vergihesaplayici.com, vergi hesaplar.</p>
 
-            <a
-                href="https://youtu.be/zsCpGUDZ85w?t=55"
-                rel="nofollow noopener noreferrer"
-                title="Osita Iheme"
-                target="_blank">
-                <video
-                    autoplay=""
-                    loop=""
-                    muted=""
-                    playsinline=""
-                    width="300"
-                    height="300">
-                    <source
-                        :src="ositaIheme.webm"
-                        type="video/webm" />
-                    <source
-                        :src="ositaIheme.mp4"
-                        type="video/mp4" />
-                </video>
-            </a>
-            <div class="caption">Osita Iheme</div>
+            <OsitaIheme />
 
             <v-divider class="my-8" />
 
@@ -36,8 +16,6 @@
 </template>
 
 <script>
-import ositaIhemeMp4 from "@/assets/video/osita-iheme.mp4";
-import ositaIhemeWebm from "@/assets/video/osita-iheme.webm";
 import openGraphImage from "@/assets/img/open-graph/index.jpg";
 
 const meta = {
@@ -60,10 +38,6 @@ export default {
                 { hid: "og:image", name: "og:image", content: openGraphImage },
                 { name: "twitter:image", content: openGraphImage }
             ]
-        },
-        ositaIheme: {
-            mp4: ositaIhemeMp4,
-            webm: ositaIhemeWebm
         }
     }),
     head() {
