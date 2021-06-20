d8393ba5fec990cdd9ce874a1a92b1e03d4eec8e
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-06-20 13:06:33 +0300

    Applied Eslint

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-	env: {
-		test: {
-			presets: [
-				[
-					"@babel/preset-env",
-					{
-						targets: {
-							node: "current"
-						}
-					}
-				]
-			],
-			plugins: [
-				"@babel/plugin-proposal-class-properties"
-			]
-		}
-	}
+    env: {
+        test: {
+            presets: [
+                [
+                    "@babel/preset-env",
+                    {
+                        targets: {
+                            node: "current"
+                        }
+                    }
+                ]
+            ],
+            plugins: [
+                "@babel/plugin-proposal-class-properties"
+            ]
+        }
+    }
 };
