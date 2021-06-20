1c31ce977251cc02a0cef3ef68518712f757204a
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-06-20 09:09:27 +0300

    Footer is not a v-footer anymore

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,12 @@
 <template>
-    <v-footer
+    <div
         v-bind="$attrs"
-        class="d-flex justify-center text-center pa-4"
-        padless="">
+        class="d-flex justify-center flex-column text-center pa-4">
         <p class="mb-4">vergihesaplayici.com, <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından açık kaynaklı olarak geliştirilmektedir</p>
-        <Ghbtns />
-    </v-footer>
+        <div>
+            <Ghbtns />
+        </div>
+    </div>
 </template>
 
 <script>
