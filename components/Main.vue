944ca1643ae9ef7af27a6d29579e96c3d75fe3d9
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-06-20 09:12:37 +0300

    Bigger padding

--- components/Main.vue
+++ components/Main.vue
@@ -1,6 +1,6 @@
 <template>
     <v-main v-bind="$attrs">
-        <v-container class="my-8">
+        <v-container class="my-8 pa-4 pa-sm-6 pa-md-8">
             <slot />
         </v-container>
     </v-main>
