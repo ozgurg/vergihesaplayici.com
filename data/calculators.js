1d58b134149c33bb516bcc66d3add54e810db200
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-11-09 22:05:26 +0300

    Update data/calculators.js

--- data/calculators.js
+++ data/calculators.js
@@ -1,27 +1,25 @@
-const calculators = {};
-
-calculators["VatCalculator"] = {
-    "icon": "mdi-label-percent",
-    "shortTitle": "Katma Değer Vergisi (KDV)",
-    "title": "Katma Değer Vergisi (KDV) Hesaplayıcı",
-    "url": "/hesaplayicilar/kdv-hesaplayici",
-    "description": "Namıdiğer her şeyin vergisini dahil ve hariç olarak hesaplayın."
-};
-
-calculators["ConsoleTaxCalculator"] = {
-    "icon": "mdi-gamepad",
-    "shortTitle": "Konsol Vergisi",
-    "title": "Konsol Vergisi Hesaplayıcı",
-    "url": "/hesaplayicilar/konsol-vergisi-hesaplayici",
-    "description": "Sadece eğlenmek istediğinizde ödemeniz gereken vergileri hesaplayın."
-};
-
-calculators["PhoneTaxCalculator"] = {
-    "icon": "mdi-cellphone",
-    "shortTitle": "Telefon Vergisi",
-    "title": "Telefon Vergisi Hesaplayıcı",
-    "url": "/hesaplayicilar/telefon-vergisi-hesaplayici",
-    "description": "Satın aldığınız bir telefonun satış fiyatına etki eden vergileri hesaplayın."
+const calculators = {
+    VatCalculator: {
+        icon: "mdi-label-percent",
+        shortTitle: "Katma Değer Vergisi (KDV)",
+        title: "Katma Değer Vergisi (KDV) Hesaplayıcı",
+        url: "/hesaplayicilar/kdv-hesaplayici",
+        description: "Namıdiğer her şeyin vergisini dahil ve hariç olarak hesaplayın."
+    },
+    ConsoleTaxCalculator: {
+        icon: "mdi-gamepad",
+        shortTitle: "Konsol Vergisi",
+        title: "Konsol Vergisi Hesaplayıcı",
+        url: "/hesaplayicilar/konsol-vergisi-hesaplayici",
+        description: "Sadece eğlenmek istediğinizde ödemeniz gereken vergileri hesaplayın."
+    },
+    PhoneTaxCalculator: {
+        icon: "mdi-cellphone",
+        shortTitle: "Telefon Vergisi",
+        title: "Telefon Vergisi Hesaplayıcı",
+        url: "/hesaplayicilar/telefon-vergisi-hesaplayici",
+        description: "Satın aldığınız bir telefonun satış fiyatına etki eden vergileri hesaplayın."
+    }
 };
 
 module.exports = calculators;
