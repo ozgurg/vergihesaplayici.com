af59b144f118b248bf7c56c827ffa4d15645a81d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-01-12 01:22:30 +0300

    Add version info to AppDrawerFooter

--- components/App/AppDrawerFooter.vue
+++ components/App/AppDrawerFooter.vue
@@ -2,9 +2,22 @@
     <div
         v-bind="$attrs"
         class="d-flex justify-center flex-column text-center pa-4">
-        <p class="mb-4">vergihesaplayici.com, <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından açık kaynaklı olarak geliştirilmektedir</p>
+        <p class="mb-2">vergihesaplayici.com v{{ version }}</p>
+
+        <p class="mb-5"><small><a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından açık kaynaklı olarak geliştirilmektedir</small></p>
+
         <div>
             <Ghbtns />
         </div>
     </div>
 </template>
+
+<script>
+import { version } from "@/package.json";
+
+export default {
+    data: () => ({
+        version
+    })
+};
+</script>
