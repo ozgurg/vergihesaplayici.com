2ba7d87eef28396af8604010085e65d742a46d74
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-04 01:32:14 +0300

    Export money-format util to utils/money-format.js

--- plugins/money-format.js
+++ plugins/money-format.js
@@ -1,11 +1,4 @@
-const moneyFormat = (price, currency) => {
-    return new Intl.NumberFormat("tr-TR",
-        {
-            style: "currency",
-            currency,
-            minimumFractionDigits: 2
-        }).format(price);
-};
+import moneyFormat from "@/utils/money-format";
 
 export default (context, inject) => {
     inject("moneyFormat", moneyFormat);
