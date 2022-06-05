340292c54846e5fbbcb5d5145a1fc688f2308361
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-06 02:00:32 +0300

    Export apps list from AppsFromDeveloperList to data/android-apps.js

--- components/AppsFromDeveloperList.vue
+++ components/AppsFromDeveloperList.vue
@@ -30,25 +30,11 @@
 </template>
 
 <script>
+import androidApps from "@/data/android-apps";
+
 export default {
     data: () => ({
-        apps: [
-            {
-                icon: "double-tap-to-lock.png",
-                title: "Double Tap To Lock",
-                url: "https://dttl.page.link/store"
-            },
-            {
-                icon: "instadownloader.png",
-                title: "InstaDownloader",
-                url: "https://instadownloader.page.link/store"
-            },
-            {
-                icon: "rebootify.png",
-                title: "Rebootify",
-                url: "https://rebootify.page.link/store"
-            }
-        ]
+        apps: androidApps
     })
 };
 </script>
