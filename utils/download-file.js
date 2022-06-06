844c1a4c9b1e98cfb97b8d81ec3e38e2b8a35293
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-06 07:46:34 +0300

    Export download file logic to an util

---
+++ utils/download-file.js
@@ -0,0 +1,13 @@
+import JsFileDownloader from "js-file-downloader";
+
+const downloadFile = (url, name) => {
+    return new JsFileDownloader({
+        url,
+        forceDesktopMode: true,
+        nativeFallbackOnError: true,
+        contentType: "image/png",
+        nameCallback: name
+    });
+};
+
+export default downloadFile;
