9042b63b9b1bba33eaf298e16abd210e89a31d40
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-08 07:55:09 +0300

    Add BaseMultiCurrencyTaxCalculator abstract method implementation test

--- calculators/BaseMultiCurrencyTaxCalculator.test.js
+++ calculators/BaseMultiCurrencyTaxCalculator.test.js
@@ -12,4 +12,19 @@ describe("calculators/BaseMultiCurrencyTaxCalculator", () => {
         expect(BaseMultiCurrencyTaxCalculator.getCalculationModeByCurrency(currency))
             .toEqual(BaseMultiCurrencyTaxCalculator.CalculationMode.BasePriceToSalePrice);
     });
+
+    it("should throw error if methods are not implemented", () => {
+        class MockMultiCurrencyTaxCalculator extends BaseMultiCurrencyTaxCalculator {
+        }
+
+        const calculator = new MockMultiCurrencyTaxCalculator({
+            price: 0,
+            exchangeRates: {},
+            calculationMode: BaseMultiCurrencyTaxCalculator.CalculationMode.BasePriceToSalePrice
+        });
+
+        expect(() => calculator.calculateTotalTaxFee()).toThrowError("Not implemented");
+        expect(() => calculator.calculateTotalTaxRate()).toThrowError("Not implemented");
+        expect(() => calculator.calculate()).toThrowError("Not implemented");
+    });
 });
