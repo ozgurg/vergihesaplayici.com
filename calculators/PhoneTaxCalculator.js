56564035b549356557e3a933191275d30f6782f4
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-28 22:05:15 +0300

    Add JSDoc to getSctRateByPrice

--- calculators/PhoneTaxCalculator.js
+++ calculators/PhoneTaxCalculator.js
@@ -5,6 +5,10 @@ import {
     calculateTotalTaxRate
 } from "@/utils/calculate-tax";
 
+/**
+ * @param {number} price
+ * @return {number}
+ */
 const getSctRateByPrice = price => {
     if (price <= 640) {
         return 25;
