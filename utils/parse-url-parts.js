938e34c02e86e179fd05824e577a18c46f0f715e
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-06-29 23:16:59 +0300

    Add parse-url-parts util (with its tests)

---
+++ utils/parse-url-parts.js
@@ -0,0 +1,10 @@
+const parseUrlParts = path => {
+    return path
+        // .replace(/^\/|\/$/g, "") // Remove leading and trailing slashes (https://regexr.com/6opbs)
+        .split("/")
+        .filter(Boolean); // .replace(...) does parts clean but since we are used .filter(Boolean), .replace(...) is not needed
+};
+
+export {
+    parseUrlParts
+};
