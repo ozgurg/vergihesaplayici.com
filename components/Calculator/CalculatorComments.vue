50fc5177e36e3e9c30cecadc5bb1dd72951ea40d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-07-10 01:20:47 +0300

    Set constant URL for CalculatorComments

--- components/Calculator/CalculatorComments.vue
+++ components/Calculator/CalculatorComments.vue
@@ -1,3 +1,18 @@
 <template>
-	<Disqus v-bind="$attrs" />
+    <Disqus
+        v-bind="$attrs"
+        :url="url" />
 </template>
+
+<script>
+import { getFullUrl } from "@/utils/get-full-url.js";
+
+export default {
+    computed: {
+        url() {
+            const vm = this;
+            return getFullUrl(vm.$route.path);
+        }
+    }
+};
+</script>
