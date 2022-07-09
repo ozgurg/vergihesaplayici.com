064330bd38a61ece0227420ace0ac8dbc296f7ce
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-07-10 01:16:31 +0300

    Add get-full-url utils (with its test and usage)

---
+++ utils/get-full-url.test.js
@@ -0,0 +1,15 @@
+import { getFullUrl } from "~/utils/get-full-url.js";
+
+describe("utils/get-full-url", () => {
+    global.process = {
+        env: {
+            APP_URL: "https://vergihesaplayici.com"
+        }
+    };
+
+    it("should return the full URL", () => {
+        const path = "/calculator";
+        const fullUrl = getFullUrl(path);
+        expect(fullUrl).toBe(`${process.env.APP_URL}${path}`);
+    });
+});
