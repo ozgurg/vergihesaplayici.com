197ef14c079938a7bdabbd0e2aeead0531f3d93b
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-07-10 03:32:16 +0300

    Make minor refactor in calculators

--- calculators/BaseTaxCalculator.js
+++ calculators/BaseTaxCalculator.js
@@ -3,6 +3,21 @@
  * @class BaseTaxCalculator
  */
 class BaseTaxCalculator {
+    /**
+     * @protected
+     * @param {function[]}functionsToCall
+     */
+    callInOrder(functionsToCall) {
+        functionsToCall.forEach(func => func.call(this));
+    }
+
+    /**
+     * @protected
+     * @param {function[]}functionsToCall
+     */
+    callInReverseOrder(functionsToCall) {
+        functionsToCall.reverse().forEach(func => func.call(this));
+    }
 }
 
 export default BaseTaxCalculator;
