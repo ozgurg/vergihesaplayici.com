fc3a6beb362ea789c3feec61e0e5964b0e773898
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-09-05 02:40:31 +0300

    Fix deprecation of querystring package

--- utils/object-to-query-string.js
+++ utils/object-to-query-string.js
@@ -1,11 +1,20 @@
-import querystring from "querystring";
-
 /**
  * @param {object} object
  * @returns {string}
  */
 const objectToQueryString = object => {
-    return querystring.stringify(object);
+    const objectToArray = [];
+    for (const [name, values] of Object.entries(object)) {
+        if (Array.isArray(values)) {
+            values.forEach(value => {
+                objectToArray.push([name, value]);
+            });
+        } else {
+            objectToArray.push([name, values]);
+        }
+    }
+
+    return new URLSearchParams(objectToArray).toString();
 };
 
 export {
