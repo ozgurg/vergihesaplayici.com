8cf89412d34b9d91cbeb9aaaf4727841eb7fca74
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-09-26 08:05:20 +0300

    Add new tests to ui.test.js

--- store/ui.test.js
+++ store/ui.test.js
@@ -15,6 +15,31 @@ describe("store/ui", () => {
         });
     });
 
-    // TODO: Add tests for mutations
+    describe("getters", () => {
+        it("[isDrawerOpen] return correct boolean value by 'drawerState'", () => {
+            expect(store.getters.isDrawerOpen({ drawerState: null })).toBe(false);
+            expect(store.getters.isDrawerOpen({ drawerState: true })).toBe(true);
+            expect(store.getters.isDrawerOpen({ drawerState: false })).toBe(false);
+        });
+    });
+
+    describe("mutations", () => {
+        it("[toggleDrawer] should toggle 'drawerState' value", () => {
+            store.mutations.toggleDrawer(state);
+            expect(state.drawerState).toBe(true);
+
+            store.mutations.toggleDrawer(state);
+            expect(state.drawerState).toBe(false);
+        });
+
+        it("[setDrawerState] should set 'drawerState'", () => {
+            store.mutations.setDrawerState(state, true);
+            expect(state.drawerState).toBe(true);
+
+            store.mutations.setDrawerState(state, false);
+            expect(state.drawerState).toBe(false);
+        });
+    });
+
     // TODO: Add tests for actions
 });
