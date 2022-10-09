110d50be188a1e71596e50b2576011b47f0931a7
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-09 18:52:55 +0300

    Refactor GitHubButton.vue

--- components/GitHubButton.vue
+++ components/GitHubButton.vue
@@ -1,21 +1,21 @@
 <template>
     <iframe
         v-bind="$attrs"
-        title="GitHub"
-        class="ghbtns"
+        title="GitHub yıldız sayısı"
+        class="vh-github-button"
         height="20"
-        src="https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true"
-        width="84" />
+        width="84"
+        src="https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true" />
 </template>
 
-<style lang="scss" scoped="">
-.ghbtns {
-	overflow: hidden;
-	border: 0;
-	padding: 0;
-	margin: 0;
-	width: 84px;
-	height: 20px;
-	display: inline-block
+<style scoped="">
+.vh-github-button {
+    overflow: hidden;
+    border: 0;
+    padding: 0;
+    margin: 0;
+    display: inline-block;
+    width: 84px;
+    height: 20px
 }
 </style>
