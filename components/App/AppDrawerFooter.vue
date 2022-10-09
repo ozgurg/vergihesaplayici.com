a1c89e2ec6749b257947c240dc548a2b255887e7
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-09 22:50:13 +0300

    Visual refactor GitHubButton.vue, AppDrawerFooter.vue

--- components/App/AppDrawerFooter.vue
+++ components/App/AppDrawerFooter.vue
@@ -1,18 +1,17 @@
 <template>
     <footer
         v-bind="$attrs"
-        class="d-flex justify-center flex-column text-center pb-3 pt-5">
-        <div class="mb-1">
+        class="d-flex align-center flex-column text-center pb-4 pt-5 px-4 grey darken-4">
+        <div class="mb-1 text-subtitle-2">
             vergihesaplayici.com v{{ version }}
         </div>
 
-        <div class="mb-5">
-            <small><a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından açık kaynaklı olarak geliştirilmektedir</small>
+        <div class="mb-4 text-body-2 text--secondary">
+            <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a>
+            tarafından açık kaynaklı olarak geliştirilmektedir
         </div>
 
-        <div>
-            <GitHubButton />
-        </div>
+        <GitHubButton />
     </footer>
 </template>
 
--- components/GitHubButton.vue
+++ components/GitHubButton.vue
@@ -1,21 +1,24 @@
 <template>
-    <iframe
-        v-bind="$attrs"
-        title="Projenin GitHub'taki yıldız sayısı"
-        class="vh-github-button"
-        height="20"
-        width="84"
-        src="https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true" />
+    <div class="vh-github-button grey darken-3 pa-2 rounded d-inline-block">
+        <iframe
+            v-bind="$attrs"
+            title="Projenin GitHub'taki yıldız sayısı"
+            height="20"
+            width="84"
+            src="https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true" />
+    </div>
 </template>
 
-<style>
+<style lang="scss">
 .vh-github-button {
-    overflow: hidden;
-    border: 0;
-    padding: 0;
-    margin: 0;
-    display: inline-block;
-    width: 84px;
-    height: 20px
+    iframe {
+        overflow: hidden;
+        border: 0;
+        padding: 0;
+        margin: 0;
+        display: block;
+        width: 84px;
+        height: 20px
+    }
 }
 </style>
