6df16b2b4c6e53f272079e22bab06c6b797d344d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-10 23:20:56 +0300

    Add minimum wage info to CalculatorMinimumWageAlert

--- components/Calculator/CalculatorMinimumWageAlert.vue
+++ components/Calculator/CalculatorMinimumWageAlert.vue
@@ -5,24 +5,36 @@
         text=""
         border="left"
         type="info">
-        Türkiye'de asgari ücretle çalışan birisi yemeden içmeden bu ürünü <b>{{ minimumWageMonthCount }}</b> ayda satın alabilir.
+        Türkiye'de asgari ücretle ({{ minimumWageFormatted }}) çalışan birisi yemeden içmeden bu ürünü
+        <b>{{ minimumWageMonthCount }}</b> ayda satın alabilir.
     </v-alert>
 </template>
 
 <script>
 import { calculateMinimumWageMonthCount } from "@/utils/calculate-minimum-wage-month-count";
+import { moneyFormat } from "@/utils/formatter.js";
 
 export default {
+    data: () => ({
+        minimumWage: process.env.MINIMUM_WAGE
+    }),
     props: {
         price: {
             type: Number,
             required: true
         }
     },
+    methods: {
+        moneyFormat
+    },
     computed: {
+        minimumWageFormatted() {
+            const vm = this;
+            return vm.moneyFormat(vm.minimumWage, "TRY");
+        },
         minimumWageMonthCount() {
             const vm = this;
-            return calculateMinimumWageMonthCount(vm.price, process.env.MINIMUM_WAGE);
+            return calculateMinimumWageMonthCount(vm.price, vm.minimumWage);
         }
     }
 };
