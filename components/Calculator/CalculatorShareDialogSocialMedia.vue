bb07a0d6bdd6b23a46a1a98d6a49a014efecbb7c
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-13 00:37:25 +0300

    Refactor CalculatorShareDialogSocialMedia.vue

--- components/Calculator/CalculatorShareDialogSocialMedia.vue
+++ components/Calculator/CalculatorShareDialogSocialMedia.vue
@@ -1,83 +1,44 @@
 <template>
     <div v-bind="$attrs">
-        <v-row
-            class="mb-2 ma-n2 d-flex justify-center"
-            no-gutters="">
-            <v-col
-                v-for="(item, key) in items"
-                :key="key"
-                class="pa-2"
-                cols="4"
-                sm="2">
-                <v-tooltip bottom="">
-                    <template #activator="{ on, attrs }">
-                        <v-btn
-                            v-on="on"
-                            v-bind="attrs"
-                            :href="item.url"
-                            :color="item.color"
-                            link=""
-                            rel="nofollow noopener noreferrer"
-                            target="_blank"
-                            block=""
-                            elevation="0"
-                            rounded=""
-                            large="">
-                            <template v-if="item.icon.endsWith('.svg')">
-                                <!-- eslint-disable vue/no-v-html -->
-                                <div v-html="require(`@/assets/img/${item.icon}?raw`)" />
-                            </template>
-                            <template v-else>
-                                <v-icon size="24">
-                                    {{ item.icon }}
-                                </v-icon>
-                            </template>
-                        </v-btn>
-                    </template>
-
-                    <span>{{ item.title }}</span>
-                </v-tooltip>
-            </v-col>
-
-            <v-col
-                v-if="isWebShareApiSupported"
-                class="pa-2"
-                cols="12"
-                sm="2">
-                <v-tooltip bottom="">
-                    <template #activator="{ on, attrs }">
-                        <v-btn
-                            @click="share()"
-                            v-on="on"
-                            v-bind="attrs"
-                            color="white"
-                            light=""
-                            block=""
-                            elevation="0"
-                            rounded=""
-                            large="">
+        <v-row>
+            <template v-for="_item in items">
+                <v-col
+                    :key="_item.title"
+                    cols="4"
+                    sm="2">
+                    <v-btn
+                        :href="_item.url"
+                        :color="_item.color"
+                        :title="_item.title"
+                        rel="nofollow noopener noreferrer"
+                        target="_blank"
+                        block=""
+                        rounded=""
+                        large="">
+                        <template v-if="isCustomIcon(_item)">
+                            <!-- eslint-disable vue/no-v-html -->
+                            <div v-html="loadCustomIconAsHtml(_item)" />
+                        </template>
+                        <template v-else>
                             <v-icon size="24">
-                                {{ icons.mdiExportVariant }}
+                                {{ _item.icon }}
                             </v-icon>
-                        </v-btn>
-                    </template>
-
-                    <span>Diğer</span>
-                </v-tooltip>
-            </v-col>
+                        </template>
+                    </v-btn>
+                </v-col>
+            </template>
         </v-row>
 
-        <div class="mt-6">
-            <v-checkbox
-                v-model="willIncludeParams"
-                hide-details=""
-                label="Hesaplayıcı parametrelerini dahil et" />
-        </div>
+        <v-checkbox
+            v-model="includeParams"
+            class="mt-8"
+            hide-details=""
+            label="Hesaplayıcı parametrelerini dahil et" />
     </div>
 </template>
 
 <script>
-import { mdiEmail, mdiExportVariant, mdiFacebook, mdiReddit, mdiTwitter, mdiWhatsapp } from "@mdi/js";
+import { mdiEmail, mdiFacebook, mdiReddit, mdiTwitter, mdiWhatsapp } from "@mdi/js";
 import { createShareUrlOfCalculator } from "@/utils/create-share-url-of-calculator.js";
 import {
     createEmailShareUrl,
@@ -90,15 +51,7 @@ import {
 
 export default {
     data: () => ({
-        icons: {
-            mdiExportVariant,
-            mdiFacebook,
-            mdiTwitter,
-            mdiWhatsapp,
-            mdiReddit,
-            mdiEmail
-        },
-        willIncludeParams: true
+        includeParams: true
     }),
     props: {
         data: {
@@ -107,14 +60,11 @@ export default {
         }
     },
     methods: {
-        share() {
-            const vm = this;
-
-            navigator.share({
-                title: document.title,
-                text: document.title,
-                url: vm.url
-            }).then().catch();
+        isCustomIcon(item) {
+            return item.icon.endsWith(".svg");
+        },
+        loadCustomIconAsHtml(item) {
+            return require(`@/assets/img/${item.icon}?raw`);
         }
     },
     computed: {
@@ -123,7 +73,7 @@ export default {
             return createShareUrlOfCalculator(
                 vm.$route.path,
                 vm.data,
-                vm.willIncludeParams
+                vm.includeParams
             );
         },
         items() {
@@ -133,19 +83,19 @@ export default {
                     url: createFacebookShareUrl(vm.url, document.title),
                     title: "Facebook'ta paylaş",
                     color: "#1877f2",
-                    icon: vm.icons.mdiFacebook
+                    icon: mdiFacebook
                 },
                 {
                     url: createTwitterShareUrl(vm.url, document.title),
                     title: "Twitter'da paylaş",
                     color: "#00acee",
-                    icon: vm.icons.mdiTwitter
+                    icon: mdiTwitter
                 },
                 {
                     url: createWhatsAppShareUrl(vm.url, document.title),
                     title: "WhatsApp ile gönder",
                     color: "#25d366",
-                    icon: vm.icons.mdiWhatsapp
+                    icon: mdiWhatsapp
                 },
                 {
                     url: createTelegramShareUrl(vm.url, document.title),
@@ -157,24 +107,21 @@ export default {
                     url: createRedditShareUrl(vm.url, document.title),
                     title: "Reddit'te paylaş",
                     color: "#FF5700",
-                    icon: vm.icons.mdiReddit
+                    icon: mdiReddit
                 },
                 {
                     url: createEmailShareUrl(vm.url, document.title),
-                    title: "E-posta gönder",
+                    title: "E-posta olarak gönder",
                     color: "#ecb064",
-                    icon: vm.icons.mdiEmail
+                    icon: mdiEmail
                 }
             ];
-        },
-        isWebShareApiSupported() {
-            return navigator.share;
         }
     }
 };
 </script>
 
-<style scoped>
+<style scoped="">
 :deep(svg) {
     vertical-align: middle;
     width: 24px;
