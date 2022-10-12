ozgurg/vergihesaplayici.com

commit 786d573a4fea15a0e94180026aa5de91734f68ac

Fix unable to reach bottom of AppDrawer on iOS devices issue

components/App/AppDrawer.vue
@@ -22,7 +22,7 @@
                 </v-list-item>
             </v-list>
 
-            <AppDrawerFooter class="mt-3" />
+            <AppDrawerFooter class="mt-15" />
         </div>
     </v-navigation-drawer>
 </template>
@@ -43,3 +43,34 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+$drawerSelector: ".v-navigation-drawer";
+
+html:has(#{$drawerSelector}--open) {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    &, *:not(#{$drawerSelector} *) {
+        overflow: hidden !important
+    }
+    body {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        top: 0;
+        overflow: hidden
+    }
+}
+
+#{$drawerSelector} {
+    height: 100% !important
+}
+</style>

commit 4cf3f7e27707a8077a49de626aa46cd7d4e05811

Fix desktop overflow issue in AppDrawer caused with 786d573a4fea15a0e94180026aa5de91734f68ac

components/App/AppDrawer.vue
@@ -45,9 +45,9 @@ export default {
 </script>
 
 <style lang="scss">
-$drawerSelector: ".v-navigation-drawer";
+$drawerSelector: "v-navigation-drawer";
 
-html:has(#{$drawerSelector}--open) {
+html:has(.#{$drawerSelector}--open.#{$drawerSelector}--is-mobile) {
     position: fixed;
     width: 100%;
     height: 100%;
@@ -55,7 +55,7 @@ html:has(#{$drawerSelector}--open) {
     left: 0;
     right: 0;
     top: 0;
-    &, *:not(#{$drawerSelector} *) {
+    &, *:not(.#{$drawerSelector} *) {
         overflow: hidden !important
     }
     body {
@@ -70,7 +70,7 @@ html:has(#{$drawerSelector}--open) {
     }
 }
 
-#{$drawerSelector} {
+.#{$drawerSelector} {
     height: 100% !important
 }
 </style>