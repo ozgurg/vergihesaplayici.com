5ef8eed4fe42d70a547b0f3f42be2cc94b9fb4fa
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-14 22:32:06 +0300

    Update layout of CalculatorShareDialog

--- components/Calculator/CalculatorShareDialog.vue
+++ components/Calculator/CalculatorShareDialog.vue
@@ -8,72 +8,58 @@
         :fullscreen="$vuetify.breakpoint.mdAndDown"
         :transition="$vuetify.breakpoint.mdAndDown ? 'dialog-bottom-transition' : 'dialog-transition'"
         width="700">
-        <v-toolbar
-            class="px-2"
-            elevation="0">
-            <v-toolbar-title class="text-center">
-                Paylaş
-            </v-toolbar-title>
+        <div class="vh-dialog__header">
+            <v-toolbar
+                class="px-2"
+                elevation="0">
+                <v-toolbar-title class="text-center">
+                    Paylaş
+                </v-toolbar-title>
 
-            <v-spacer />
+                <v-spacer />
 
-            <v-btn
-                @click="close()"
-                icon="">
-                <v-icon>{{ icons.mdiClose }}</v-icon>
-            </v-btn>
-        </v-toolbar>
+                <v-btn
+                    @click="close()"
+                    icon="">
+                    <v-icon>{{ icons.mdiClose }}</v-icon>
+                </v-btn>
+            </v-toolbar>
 
-        <div class="vh-dialog__content py-10">
-            <v-expansion-panels
-                :value="0"
-                flat=""
-                tile=""
-                accordion=""
-                focusable="">
-                <v-expansion-panel>
-                    <v-expansion-panel-header>
-                        <div class="d-flex align-center">
-                            <v-icon
-                                class="mr-4"
-                                left="">
-                                {{ icons.mdiLink }}
-                            </v-icon>
-                            Bağlantı
-                        </div>
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                        <div class="pl-2 pr-2 pt-8 pb-4">
-                            <CalculatorShareDialogUrl :data="formData" />
-                        </div>
-                    </v-expansion-panel-content>
+            <v-tabs
+                v-model="currentTab"
+                class="vh-share-tabs"
+                grow=""
+                background-color="transparent">
+                <v-tab>
+                    <v-icon left>
+                        {{ icons.mdiLink }}
+                    </v-icon>
+                    <span>Bağlantı</span>
+                </v-tab>
+                <v-tab>
+                    <v-icon left>
+                        {{ icons.mdiCellphoneScreenshot }}
+                    </v-icon>
+                    <span>Ekran görüntüsü</span>
+                </v-tab>
+            </v-tabs>
+        </div>
 
-                    <v-divider />
-                </v-expansion-panel>
+        <div class="vh-dialog__content">
+            <v-tabs-items
+                :value="currentTab"
+                touchless="">
+                <v-tab-item class="pa-5 pa-lg-8">
+                    <CalculatorShareDialogUrl :data="formData" />
+                </v-tab-item>
 
-                <v-expansion-panel>
-                    <v-expansion-panel-header>
-                        <div class="d-flex align-center">
-                            <v-icon
-                                class="mr-4"
-                                left="">
-                                {{ icons.mdiCellphoneScreenshot }}
-                            </v-icon>
-                            Ekran görüntüsü
-                        </div>
-                    </v-expansion-panel-header>
-                    <v-expansion-panel-content>
-                        <div class="pl-2 pr-2 pt-8 pb-4">
-                            <CalculatorShareDialogScreenshot
-                                :data="screenshotData"
-                                :title="title"
-                                :matching-presets="matchingPresets" />
-                        </div>
-                    </v-expansion-panel-content>
-
-                    <v-divider />
-                </v-expansion-panel>
-            </v-expansion-panels>
+                <v-tab-item class="pa-5 pa-lg-8">
+                    <CalculatorShareDialogScreenshot
+                        :data="screenshotData"
+                        :title="title"
+                        :matching-presets="matchingPresets" />
+                </v-tab-item>
+            </v-tabs-items>
         </div>
     </v-dialog>
 </template>
@@ -120,3 +106,11 @@ export default {
     }
 };
 </script>
+
+<style lang="scss" scoped="">
+@import "~vuetify/src/styles/styles.sass";
+
+.vh-share-tabs {
+    background: map-get($material-dark-elevation-colors, "4")
+}
+</style>
