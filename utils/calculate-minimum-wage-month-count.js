cadad5609f4628bf67b4a5b9e93fac48cd4d8470
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-14 20:54:38 +0300

    Add calculateMinimumWageDayCount util with its tests

--- utils/calculate-minimum-wage-month-count.js
+++ utils/calculate-minimum-wage-month-count.js
@@ -7,6 +7,16 @@ const calculateMinimumWageMonthCount = (price, minimumMage) => {
     return (price / minimumMage).toFixed(1);
 };
 
+/**
+ * @param {number} price
+ * @param {number} minimumWage
+ * @returns {string}
+ */
+const calculateMinimumWageDayCount = (price, minimumWage) => {
+    return ((price / minimumWage) * 30).toFixed(0);
+};
+
 export {
-    calculateMinimumWageMonthCount
+    calculateMinimumWageMonthCount,
+    calculateMinimumWageDayCount
 };
--- utils/calculate-minimum-wage-month-count.test.js
+++ utils/calculate-minimum-wage-month-count.test.js
@@ -1,15 +1,37 @@
-import { calculateMinimumWageMonthCount } from "@/utils/calculate-minimum-wage-month-count.js";
+import {
+    calculateMinimumWageDayCount,
+    calculateMinimumWageMonthCount
+} from "@/utils/calculate-minimum-wage-month-count.js";
 
 describe("utils/calculate-minimum-wage-month-count", () => {
     const minimumWage = 5_000;
 
-    it("should return 1.50 if price is 7.500", () => {
-        const price = 7_500;
-        expect(calculateMinimumWageMonthCount(price, minimumWage)).toBe("1.5");
+    describe("calculateMinimumWageMonthCount", () => {
+        it("should return 1.50 if price is 7.500", () => {
+            const price = 7_500;
+            expect(calculateMinimumWageMonthCount(price, minimumWage)).toBe("1.5");
+        });
+
+        it("should return 10.00 if price is 50.000", () => {
+            const price = 50_000;
+            expect(calculateMinimumWageMonthCount(price, minimumWage)).toBe("10.0");
+        });
     });
 
-    it("should return 10.00 if price is 50.000", () => {
-        const price = 50_000;
-        expect(calculateMinimumWageMonthCount(price, minimumWage)).toBe("10.0");
+    describe("calculateMinimumWageDayCount", () => {
+        it("should return 15 if price is half of the minimum wage", () => {
+            const price = minimumWage / 2;
+            expect(calculateMinimumWageDayCount(price, minimumWage)).toBe("15");
+        });
+
+        it("should return 30 if price is same with the minimum wage", () => {
+            const price = minimumWage;
+            expect(calculateMinimumWageDayCount(price, minimumWage)).toBe("30");
+        });
+
+        it("should return 1 for one day price of the minimum wage", () => {
+            const price = minimumWage / 30;
+            expect(calculateMinimumWageDayCount(price, minimumWage)).toBe("1");
+        });
     });
 });
