8798427faf91a5c8bc20e6697e09ff060175e731
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-15 02:54:47 +0300

    Set empty GitHubStarButton iframe src if NODE_ENV isn't production to prevent unnecessary requests

--- components/GitHubStarButton.vue
+++ components/GitHubStarButton.vue
@@ -3,12 +3,24 @@
         v-bind="$attrs"
         class="vh-github-button grey darken-3 pa-2 rounded d-inline-block">
         <iframe
+            :src="src"
             title="Projenin GitHub'taki yıldız sayısı"
             height="20"
-            width="84"
-            src="https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true" />
+            width="84" />
     </div>
 </template>
+
+<script>
+export default {
+    computed: {
+        src() {
+            return process.env.NODE_ENV === "production" ?
+                "https://ghbtns.com/github-btn.html?user=ozgurg&repo=vergihesaplayici.com&type=star&count=true" :
+                false;
+        }
+    }
+};
+</script>
 
 <style>
 .vh-github-button iframe {
