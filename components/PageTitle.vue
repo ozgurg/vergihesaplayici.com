3f345fd60fe79b848311e42b2890d0be8e739cd6
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-14 21:17:19 +0300

    Update text size of PageTitle

--- components/PageTitle.vue
+++ components/PageTitle.vue
@@ -2,7 +2,7 @@
     <div
         v-bind="$attrs"
         class="mb-16">
-        <h1 class="text-md-h3 text-h4 mb-4 font-weight-bold">
+        <h1 class="text-h4 text-lg-h3 mb-4 font-weight-bold">
             <slot />
         </h1>
 
