c7cc77c6d838e8577a7108fc5d34715fbebfbce5
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-16 01:52:18 +0300

    Update text in AppFooter

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -6,7 +6,7 @@
 
         <div class="mb-4 text-body-2 text--secondary">
             <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a>
-            tarafından açık kaynaklı olarak geliştirilmektedir
+            tarafından açık kaynaklı olarak geliştirilmektedir.
         </div>
 
         <GitHubStarButton />
