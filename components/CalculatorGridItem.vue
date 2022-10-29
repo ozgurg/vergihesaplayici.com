b3f91a7c1dfcfdeb25e44fa3aa35399241d3445d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-29 15:45:54 +0300

    Visual update CalculatorGridItem

--- components/CalculatorGridItem.vue
+++ components/CalculatorGridItem.vue
@@ -1,18 +1,21 @@
 <template>
     <v-col
+        class="vh-calculator-grid__item"
         cols="12"
         md="6">
         <v-card
             :to="url"
             outlined=""
-            class="px-6 py-8 pa-sm-8 d-flex flex-column justify-space-between h-100">
-            <v-icon
-                class="primary--text"
-                size="56">
-                {{ icon }}
-            </v-icon>
+            class="px-6 py-8 pa-sm-8 d-flex flex-column justify-space-between align-start h-100">
+            <div class="vh-calculator-grid__item__icon d-inline-block pa-5 rounded-pill">
+                <v-icon
+                    class="primary--text"
+                    size="40">
+                    {{ icon }}
+                </v-icon>
+            </div>
 
-            <h2 class="text-h6 mt-4 mb-3 text--primary h-100">
+            <h2 class="text-h6 mt-5 mb-3 text--primary h-100">
                 {{ title }}
             </h2>
 
@@ -46,3 +49,9 @@ export default {
     }
 };
 </script>
+
+<style lang="scss">
+.vh-calculator-grid__item__icon {
+    background: transparentize(#88DF95, .92)
+}
+</style>
