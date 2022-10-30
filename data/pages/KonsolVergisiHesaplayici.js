5374ce4b5717e72d0f563b2a13c042111f6aad5d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-10-30 21:03:31 +0300

    Move presets from pages/.../konsol-vergisi-hesaplayici.vue to data/.../KonsolVergisiHesaplayici.js

--- data/pages/KonsolVergisiHesaplayici.js
+++ data/pages/KonsolVergisiHesaplayici.js
@@ -2,6 +2,16 @@ import { mdiController } from "@mdi/js";
 import { buildHeadTags } from "@/utils/build-head-tags.js";
 import Hesaplayicilar from "@/data/pages/Hesaplayicilar.js";
 
+const presets = [
+    // PS5: https://blog.playstation.com/2022/08/25/ps5-price-to-increase-in-select-markets-due-to-global-economic-environment-including-high-inflation-rates/
+    // Nintendo Switch: https://en.wikipedia.org/wiki/Nintendo_Switch#cite_note-polygon_oled_announce-178
+    { id: 1, title: "Xbox Series S (512GB)", form: { price: 299, currency: "EUR" } },
+    { id: 2, title: "Xbox Series X (1TB)", form: { price: 499, currency: "EUR" } },
+    { id: 3, title: "PlayStation 5 Digital Edition (825GB)", form: { price: 449.99, currency: "EUR" } },
+    { id: 4, title: "PlayStation 5 (825GB)", form: { price: 549.99, currency: "EUR" } },
+    { id: 5, title: "Nintendo Switch OLED (64GB)", form: { price: 349.99, currency: "USD" } }
+];
+
 export default (() => {
     const icon = mdiController;
     const title = "Konsol Vergisi Hesaplayıcı";
@@ -30,3 +40,7 @@ export default (() => {
         summary
     };
 })();
+
+export {
+    presets
+};
--- pages/hesaplayicilar/konsol-vergisi-hesaplayici.vue
+++ pages/hesaplayicilar/konsol-vergisi-hesaplayici.vue
@@ -79,7 +79,7 @@ import {
     findCalculatorMatchingPresets
 } from "@/utils/find-calculator-matching-presets.js";
 import { moneyFormat } from "@/utils/formatter.js";
-import page from "@/data/pages/KonsolVergisiHesaplayici.js";
+import page, { presets } from "@/data/pages/KonsolVergisiHesaplayici.js";
 
 export default {
     head() {
@@ -88,15 +88,7 @@ export default {
     data: () => ({
         page,
         ui: {
-            presets: [
-                // PS5: https://blog.playstation.com/2022/08/25/ps5-price-to-increase-in-select-markets-due-to-global-economic-environment-including-high-inflation-rates/
-                // Nintendo Switch: https://en.wikipedia.org/wiki/Nintendo_Switch#cite_note-polygon_oled_announce-178
-                { id: 1, title: "Xbox Series S (512GB)", form: { price: 299, currency: "EUR" } },
-                { id: 2, title: "Xbox Series X (1TB)", form: { price: 499, currency: "EUR" } },
-                { id: 3, title: "PlayStation 5 Digital Edition (825GB)", form: { price: 449.99, currency: "EUR" } },
-                { id: 4, title: "PlayStation 5 (825GB)", form: { price: 549.99, currency: "EUR" } },
-                { id: 5, title: "Nintendo Switch OLED (64GB)", form: { price: 349.99, currency: "USD" } }
-            ],
+            presets,
             tab: 1
         },
         form: {
