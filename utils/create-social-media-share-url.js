1a85318cfd4e41149536bd7e28366e2eaf125cf6
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-12-10 20:15:04 +0300

    refactor: minor refactor about utils

--- utils/create-social-media-share-url.js
+++ utils/create-social-media-share-url.js
@@ -1,33 +1,35 @@
-const createFacebookShareUrl = (url, title) => createSocialMediaShareUrl("facebook", url, title);
-const createTwitterShareUrl = (url, title) => createSocialMediaShareUrl("twitter", url, title);
-const createWhatsAppShareUrl = (url, title) => createSocialMediaShareUrl("whatsapp", url, title);
-const createTelegramShareUrl = (url, title) => createSocialMediaShareUrl("telegram", url, title);
-const createRedditShareUrl = (url, title) => createSocialMediaShareUrl("reddit", url, title);
+/**
+ * @param {string} url
+ * @returns {string}
+ */
+const createFacebookShareUrl = url => `https://www.facebook.com/sharer/sharer.php?u=${url}`;
 
 /**
- * @param {string} site
  * @param {string} url
  * @param {string} title
  * @returns {string}
  */
-const createSocialMediaShareUrl = (site, url, title) => {
-    switch (site) {
-        case "facebook":
-            return `https://www.facebook.com/sharer/sharer.php?u=${url}`;
+const createTwitterShareUrl = (url, title) => `https://twitter.com/intent/tweet?url=${url}&text=${title}`;
 
-        case "twitter":
-            return `https://twitter.com/intent/tweet?url=${url}&text=${title}`;
+/**
+ * @param {string} url
+ * @returns {string}
+ */
+const createWhatsAppShareUrl = url => `https://wa.me/?text=${url}`;
 
-        case "whatsapp":
-            return `https://wa.me/?text=${url}`;
+/**
+ * @param {string} url
+ * @param {string} title
+ * @returns {string}
+ */
+const createTelegramShareUrl = (url, title) => `https://telegram.me/share/url?url=${url}&text=${title}`;
 
-        case "telegram":
-            return `https://telegram.me/share/url?url=${url}&text=${title}`;
-
-        case "reddit":
-            return `https://www.reddit.com/submit?url=${url}&title=${title}`;
-    }
-};
+/**
+ * @param {string} url
+ * @param {string} title
+ * @returns {string}
+ */
+const createRedditShareUrl = (url, title) => `https://www.reddit.com/submit?url=${url}&title=${title}`;
 
 export {
     createFacebookShareUrl,
