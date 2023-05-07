d4457271b8ec6156231275fc9fd05cdc458cfb49
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-05-07 04:51:35 +0300

    ui: update AppFooter, remove GitHubStarButton

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,13 +1,8 @@
 <template>
     <div>
         <v-divider />
-        <footer class="d-flex align-center flex-column text-center py-6 px-4">
-            <div class="mb-3 text-body-2 text--secondary">
-                <b>v{{ version }}</b> – <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a>
-                tarafından açık kaynaklı olarak geliştirilmektedir
-            </div>
-
-            <GitHubStarButton />
+        <footer class="text-center pa-6 text-body-2 text--secondary">
+            <b>v{{ version }}</b> – <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından <a rel="nofollow noopener noreferrer" target="_blank" href="https://github.com/ozgurg/vergihesaplayici.com">açık kaynaklı</a> olarak geliştirilmektedir
         </footer>
     </div>
 </template>
