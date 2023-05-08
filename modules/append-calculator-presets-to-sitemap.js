0d0855c1e1cf0c08c0353c61cf9758d4195fa78e
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-05-09 02:48:07 +0300

    nuxt: fix sitemap generator

--- modules/append-calculator-presets-to-sitemap.js
+++ modules/append-calculator-presets-to-sitemap.js
@@ -3,16 +3,15 @@
  */
 export default function AppendCalculatorPresetsToSitemap() {
     this.nuxt.hook("generate:done", generator => {
-        const generatedRoutes = [...generator.generatedRoutes]; // "generatedRoutes" is a Set. We convert it to an array.
-        this.nuxt.options.sitemap.routes = generatedRoutes
-            .filter(route => {
-                return route.match(/(\/telefon-vergisi-hesaplayici\/(.*)\/)/) ||
-                    route.match(/(\/konsol-vergisi-hesaplayici\/(.*)\/)/);
-            })
-            .map(url => ({
-                url,
-                changefreq: "daily",
-                priority: 0.9
-            }));
+        this.nuxt.options.sitemap.routes = [
+            ...generator.generatedRoutes
+        ].filter(route => {
+            return route.match(/(\/telefon-vergisi-hesaplayici\/(.*)\/)/) ||
+                route.match(/(\/konsol-vergisi-hesaplayici\/(.*)\/)/);
+        }).map(url => ({
+            url,
+            changefreq: "daily",
+            priority: 0.9
+        }));
     });
 }
