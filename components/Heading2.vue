0806946b7aaa317b1b27467fcf24b6407a02aae7
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-09 15:29:29 +0300

    components(Heading2): add hover style

--- components/Heading2.vue
+++ components/Heading2.vue
@@ -3,7 +3,7 @@
         <template v-if="to">
             <nuxt-link
                 :to="to"
-                class="text-decoration-none white--text">
+                class="text-decoration-none">
                 <slot />
             </nuxt-link>
         </template>
@@ -25,9 +25,21 @@ export default {
 </script>
 
 <style lang="scss">
+@import "@/assets/scss/mixins.scss";
+
 .vh-heading-2 {
     position: relative;
     padding-inline-start: 1.125rem;
+    color: #fff;
+
+    a {
+        color: inherit;
+        @include vh-real-hover {
+            &:hover {
+                color: #88DF95
+            }
+        }
+    }
 
     &::before {
         position: absolute;
