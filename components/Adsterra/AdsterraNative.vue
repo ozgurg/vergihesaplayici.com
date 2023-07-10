44dcc1dbd6a4722d00cd2cc4b2f47807f395bc2b
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-11 00:10:38 +0300

    components(AdsterraNative): add "REKLAM" caption

--- components/Adsterra/AdsterraNative.vue
+++ components/Adsterra/AdsterraNative.vue
@@ -2,8 +2,13 @@
     <div
         ref="root"
         class="vh-an">
-        <div ref="scriptContainer" />
-        <div :id="containerId" />
+        <span class="vh-an__caption text-caption text-uppercase">
+            Reklam
+        </span>
+        <div class="vh-an__inner">
+            <div ref="scriptContainer" />
+            <div :id="containerId" />
+        </div>
     </div>
 </template>
 
@@ -67,11 +72,30 @@ export default {
 </script>
 
 <style lang="scss" scoped="">
+$vh-an-border-radius: .5rem;
+
 .vh-an {
-    background: rgba(#fff, .05);
-    border-radius: .5rem;
-    overflow: hidden;
-    display: block;
-    max-width: 100%
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    max-width: 100%;
+
+    &__caption {
+        background: rgba(#fff, .12);
+        display: inline-flex;
+        align-self: flex-end;
+        padding: .125rem .375rem;
+        border-radius: $vh-an-border-radius $vh-an-border-radius 0 0
+    }
+
+    &__inner {
+        background: rgba(#fff, .04);
+        border-radius: $vh-an-border-radius 0 $vh-an-border-radius $vh-an-border-radius;
+        overflow: hidden;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        min-height: 274px
+    }
 }
 </style>
