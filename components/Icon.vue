42f3cafc8d9624237205b5813a7dac5ec8c14b84
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-15 23:25:30 +0300

    components: create new Icon component

---
+++ components/Icon.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="vh-icon">
+        <template v-if="isCustomIcon(icon)">
+            <!-- eslint-disable vue/no-v-html -->
+            <div v-html="loadCustomIconAsHtml(icon)" />
+        </template>
+        <template v-else>
+            <v-icon
+                :color="color"
+                :size="size">
+                {{ icon }}
+            </v-icon>
+        </template>
+    </div>
+</template>
+
+<script>
+import { isCustomIcon, loadCustomIconAsHtml } from "@/utils/custom-icon.js";
+
+export default {
+    props: {
+        icon: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: Number,
+            default: 24
+        }
+    },
+    methods: {
+        isCustomIcon,
+        loadCustomIconAsHtml
+    }
+};
+</script>
+
+<style lang="scss" scoped="">
+.vh-icon {
+    > div {
+        width: 24px;
+        height: 24px
+    }
+    :deep(svg) {
+        fill: v-bind(color)
+    }
+}
+</style>
