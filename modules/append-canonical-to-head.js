3d5fc67eafd5f8aea8809a97269b77b3c22b55a1
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2023-07-20 07:56:34 +0300

    app: replace external nuxt-content module with internal module

---
+++ modules/append-canonical-to-head.js
@@ -0,0 +1,22 @@
+// @ alias is not working
+import * as cheerio from "cheerio";
+import { getFullUrl } from "./../utils/get-full-url.js";
+
+/**
+ * @class
+ */
+export default function AppendCanonicalToHead() {
+    this.nuxt.hook("generate:page", page => {
+        // Inspired by https://github.com/littlegiants/nuxt-canonical
+
+        const $ = cheerio.load(page.html);
+        const head = $("head");
+
+        const fullUrl = getFullUrl(page.route);
+
+        head.append(`<link rel="canonical" href="${fullUrl}" />`);
+        head.append(`<meta property="og:url" content="${fullUrl}" />`);
+
+        page.html = $.html();
+    });
+}
