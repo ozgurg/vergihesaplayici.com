dab5927f51f99fa01d44028eecc8e4b29f9929a8
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-01-02 23:50:46 +0300

    ux: toggle mobile footer by using CSS instead JS

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,20 +8,8 @@
 
         <MainContainer>
             <Nuxt />
-        </MainContainer>
 
-        <!-- We use tag="div", because AppFooter is already <footer />-->
-        <template v-if="$vuetify.breakpoint.smAndDown">
-            <v-footer
-                app=""
-                tag="div"
-                inset=""
-                padless=""
-                class="mt-16 pa-8 d-block"
-                color="transparent"
-                absolute="">
-                <AppFooter />
-            </v-footer>
-        </template>
+            <AppFooter class="d-block d-md-none mt-16" />
+        </MainContainer>
     </v-app>
 </template>
