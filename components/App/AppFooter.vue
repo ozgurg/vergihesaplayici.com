3af6043864d00cf55f5692413da2cc82fdd5fa95
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-01-08 00:55:27 +0300

    components(AppFooter): refactor

--- components/App/AppFooter.vue
+++ components/App/AppFooter.vue
@@ -1,16 +1,28 @@
 <template>
+    <!--
+    To enhance readability, formatter:on and formatter:off comments have been added to disable the WebStorm formatter for the sentence.
+    -->
     <footer class="text-center text-body-2 text--secondary">
-        <a href="https://github.com/ozgurg" rel="nofollow noopener noreferrer" target="_blank">@ozgurg</a> tarafından <a rel="nofollow noopener noreferrer" target="_blank" href="https://github.com/ozgurg/vergihesaplayici.com">açık kaynaklı</a> olarak geliştirilmektedir
-        <b class="mt-2 d-block">v{{ version }}</b>
+        <!-- @formatter:off -->
+        <a :href="authorUrl" rel="nofollow noopener noreferrer" target="_blank">@{{ authorUsername }}</a> tarafından <a :href="repositoryUrl" rel="nofollow noopener noreferrer" target="_blank">açık kaynaklı</a> olarak geliştirilmektedir
+        <!-- @formatter:on -->
+
+        <span class="font-weight-bold mt-2 d-block">
+            v{{ version }}
+        </span>
     </footer>
 </template>
 
-<script>
-import { version } from "@/package.json";
+<script setup>
+// FIXME:
+// Importing with object destructuring in "script setup" without using "as" causes WebStorm to not detect their usage within the template.
+// This can be refactored when it's fixed.
+import packageJson from "@/package.json";
 
-export default {
-    data: () => ({
-        version
-    })
-};
+const authorUrl = packageJson.author.url;
+const authorUsername = packageJson.author.url.split("/")[3];
+
+const repositoryUrl = packageJson.repository.url;
+
+const version = packageJson.version;
 </script>
