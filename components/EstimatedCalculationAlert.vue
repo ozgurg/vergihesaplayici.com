e82b5b26b9a86dd1ecde9516e68e7bc10bdcdd4e
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-01-09 06:45:58 +0300

    components(EstimatedCalculationAlert): update text

--- components/EstimatedCalculationAlert.vue
+++ components/EstimatedCalculationAlert.vue
@@ -1,7 +1,6 @@
 <template>
     <vh-alert type="warning">
-        Ürünlerin ülkeye giriş fiyatını veya menşei ülkenin vergilendirmesini bilmediğimiz için yurt dışı satış
-        fiyatından <b>tahmini olarak</b> hesaplanmıştır. Gerçek satış
-        fiyatı farklı olabilir.
+        Ürünlerin ülkeye giriş fiyatını ve menşei ülkenin vergilendirmesini bilmediğimiz için, yurt dışı satış fiyatı
+        üzerinden <b>tahmini olarak</b> hesaplanmıştır. Gerçek satış fiyatı farklılık gösterebilir.
     </vh-alert>
 </template>
