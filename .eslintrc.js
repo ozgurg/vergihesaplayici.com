79ddb17d44b648214e922e62af2a9ffaa196ec46
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-03-10 17:22:36 +0300

    style: uninstall eslint-plugin-jsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,6 @@ module.exports = {
         "plugin:nuxt/recommended",
         "google"
     ],
-    plugins: [
-        "jsdoc"
-    ],
     parserOptions: {
         parser: "@babel/eslint-parser",
         requireConfigFile: false
@@ -108,52 +105,6 @@ module.exports = {
             }
         }],
         "vue/multi-word-component-names": "off",
-        "valid-jsdoc": 0,
-        "jsdoc/check-access": 1,
-        "jsdoc/check-alignment": 1,
-        "jsdoc/check-indentation": 1,
-        "jsdoc/check-line-alignment": 1,
-        "jsdoc/check-param-names": 1,
-        "jsdoc/check-property-names": 1,
-        "jsdoc/check-syntax": 1,
-        "jsdoc/check-tag-names": 1,
-        "jsdoc/check-types": 1,
-        "jsdoc/check-values": 1,
-        "jsdoc/empty-tags": 1,
-        "jsdoc/implements-on-classes": 1,
-        "jsdoc/match-description": 1,
-        "jsdoc/multiline-blocks": 1,
-        "jsdoc/newline-after-description": 0,
-        "jsdoc/no-bad-blocks": 1,
-        "jsdoc/no-defaults": 1,
-        "jsdoc/no-missing-syntax": 0,
-        "jsdoc/no-multi-asterisks": 1,
-        "jsdoc/no-restricted-syntax": 0,
-        "jsdoc/no-types": 0,
-        "jsdoc/no-undefined-types": 1,
-        "jsdoc/require-asterisk-prefix": 1,
-        "jsdoc/require-description": 0,
-        "jsdoc/require-description-complete-sentence": 0,
-        "jsdoc/require-example": 0,
-        "jsdoc/require-file-overview": 0,
-        "jsdoc/require-hyphen-before-param-description": 1,
-        "jsdoc/require-jsdoc": 1,
-        "jsdoc/require-param": 1,
-        "jsdoc/require-param-description": 0,
-        "jsdoc/require-param-name": 1,
-        "jsdoc/require-param-type": 1,
-        "jsdoc/require-property": 1,
-        "jsdoc/require-property-description": 1,
-        "jsdoc/require-property-name": 1,
-        "jsdoc/require-property-type": 1,
-        "jsdoc/require-returns": 1,
-        "jsdoc/require-returns-check": 1,
-        "jsdoc/require-returns-description": 0,
-        "jsdoc/require-returns-type": 1,
-        "jsdoc/require-throws": 1,
-        "jsdoc/require-yields": 1,
-        "jsdoc/require-yields-check": 1,
-        "jsdoc/tag-lines": 0,
-        "jsdoc/valid-types": 1
+        "valid-jsdoc": 0
     }
 };
