feef1b236b3bf3f6976a0dc7818410ac47dc66ff
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-03-17 03:37:38 +0300

    style: minor code quality changes

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -1,31 +1,26 @@
-/* eslint-disable import/no-mutable-exports */
 import { initializeApp } from "firebase/app";
 import { initializeAnalytics } from "firebase/analytics";
 import { initializePerformance } from "firebase/performance";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
-let firebaseApp = null;
+const firebaseApp = initializeApp({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+});
 
-if (process.env.NODE_ENV === "production") {
-    firebaseApp = initializeApp({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    });
+initializeAnalytics(firebaseApp);
 
-    initializeAnalytics(firebaseApp);
+initializePerformance(firebaseApp);
 
-    initializePerformance(firebaseApp);
-
-    initializeAppCheck(firebaseApp, {
-        provider: new ReCaptchaV3Provider(process.env.RECAPTCHA_V3_PUBLIC_KEY),
-        isTokenAutoRefreshEnabled: true
-    });
-}
+initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaV3Provider(process.env.RECAPTCHA_V3_PUBLIC_KEY),
+    isTokenAutoRefreshEnabled: true
+});
 
 export default firebaseApp;
