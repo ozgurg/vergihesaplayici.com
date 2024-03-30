d78b205caf43f8b66b3c2d1997eb111037f3d70d
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-03-31 01:20:45 +0300

    components(affordability-alert): refactor

--- components/affordability-alert.vue
+++ components/affordability-alert.vue
@@ -1,8 +1,7 @@
 <template>
-    <!-- DO NOT use `v-html` on the root element -->
     <vh-alert type="info">
-        <!-- eslint-disable vue/no-v-text-v-html-on-component vue/no-v-html -->
-        <div v-html="text" />
+        Türkiye'de asgari ücretle ({{ formattedMinimumWage }}) çalışan birisi yemeden içmeden bu ürünü <span
+        class="text-h6 font-weight-bold">{{ count }}</span> {{ timeUnit }} maaşıyla satın alabilir.
     </vh-alert>
 </template>
 
@@ -21,11 +20,8 @@ const props = defineProps({
 
 const minimumWageMonthCount = calculateMinimumWageMonthCount(props.price, MINIMUM_WAGE);
 const minimumWageDayCount = calculateMinimumWageDayCount(props.price, MINIMUM_WAGE);
-const formattedMinimumWage = moneyFormat(MINIMUM_WAGE, "TRY");
 
-const text = (() => {
-    const count = minimumWageMonthCount > 1 ? minimumWageMonthCount : minimumWageDayCount;
-    const timeUnit = minimumWageMonthCount > 1 ? "aylık" : "günlük";
-    return `Türkiye'de asgari ücretle (${formattedMinimumWage}) çalışan birisi yemeden içmeden bu ürünü <span class="text-h6 font-weight-bold">${count}</span> ${timeUnit} maaşıyla satın alabilir.`;
-})();
+const formattedMinimumWage = moneyFormat(MINIMUM_WAGE, "TRY");
+const count = minimumWageMonthCount > 1 ? minimumWageMonthCount : minimumWageDayCount;
+const timeUnit = minimumWageMonthCount > 1 ? "aylık" : "günlük";
 </script>
