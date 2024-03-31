e5eeae7b44984178f7fda1be1e5494fc06d73442
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2024-03-31 12:04:44 +0300

    components(form-row): replace v-row/v-col with just div

--- components/form-row.vue
+++ components/form-row.vue
@@ -1,23 +1,22 @@
 <template>
-    <v-row
+    <div
         :class="{
             'horizontal': props.direction === 'horizontal',
             'highlighted': isHighlighted
         }"
-        no-gutters=""
-        class="form-row flex-column flex-sm-row">
-        <v-col>
-            <template v-if="props.label">
+        class="form-row">
+        <div>
+            <template v-if="props.label !== null">
                 <label>
                     {{ props.label }}
                 </label>
             </template>
-        </v-col>
+        </div>
 
-        <v-col>
+        <div>
             <slot v-if="$slots.default" />
-        </v-col>
-    </v-row>
+        </div>
+    </div>
 </template>
 
 <script setup="">
@@ -43,33 +42,37 @@ const props = defineProps({
 .form-row {
     --vh-form-row-label-col-width: 100%;
     --vh-form-row-label-justify-content: flex-start;
+    display: flex;
+    flex-flow: column nowrap;
     gap: .375rem;
-
     &.horizontal {
+        flex-direction: column;
         @media #{map-get($display-breakpoints, "sm-and-up")} {
             --vh-form-row-label-col-width: 170px; // Optimized for "TRT bandrolü (€20,00)" text
             --vh-form-row-label-justify-content: flex-end;
+            flex-direction: row;
             gap: 1rem
         }
     }
-
     &.highlighted {
         label {
             font-weight: 700;
             color: $vh-color-primary
         }
     }
-
-    & > .col:first-child {
+    & > div:first-child {
         flex: 0 0 var(--vh-form-row-label-col-width);
         max-width: var(--vh-form-row-label-col-width);
-
         label {
             height: min(#{$text-field-filled-full-width-outlined-slot-min-height}, 100%);
             display: flex;
             align-items: center;
             justify-content: var(--vh-form-row-label-justify-content)
         }
+    }
+    & > div:last-child {
+        flex: 1;
+        width: 100%
     }
 }
 </style>
