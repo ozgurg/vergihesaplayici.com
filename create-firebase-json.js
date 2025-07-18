5f1f0e011c0de3791d6b01226ce85984f6da381b
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2025-07-18 21:44:22 +0300

    feat: add security headers

--- create-firebase-json.js
+++ create-firebase-json.js
@@ -114,6 +114,38 @@ const firebaseJson = {
                     { // https://hstspreload.org/
                         key: "Strict-Transport-Security",
                         value: "max-age=63072000; includeSubDomains; preload"
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN"
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff"
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "strict-origin-when-cross-origin"
+                    },
+                    { // Deprecated header
+                        key: "X-XSS-Protection",
+                        value: "1; mode=block"
+                    },
+                    {
+                        key: "X-Permitted-Cross-Domain-Policies",
+                        value: "none"
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp"
+                    },
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin"
+                    },
+                    {
+                        key: "Cross-Origin-Resource-Policy",
+                        value: "same-origin"
                     }
                 ]
             },
