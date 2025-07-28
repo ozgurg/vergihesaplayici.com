0cf34346520affb1c95304897650b9bde79d5567
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2025-07-29 00:51:31 +0300

    content: update calculator result list labels

--- src/domains/vergini-olustur/utils/build-result-list.ts
+++ src/domains/vergini-olustur/utils/build-result-list.ts
@@ -1,40 +1,48 @@
 import type { CalculationResults, ResultList } from "@/domains/vergini-olustur/types.js";
 import { RateType } from "@/domains/vergini-olustur/types.js";
-import { formatNumber } from "@/utils/formatter.js";
+import { normalizePrice } from "@/utils/formatter.js";
 
 export const buildResultList = (results: CalculationResults): ResultList => {
     const { taxFees, taxRates, prices } = results;
-    return [
-        {
-            key: "taxFree",
-            label: "Vergisiz fiyat",
-            value: formatMoney(prices.taxFree, "TRY")
-        },
-        ...taxFees.items.map((_taxFeeItem, _index) => {
-            const name = `${_taxFeeItem.taxItem.name || `${_index + 1}. vergi`}`;
+    const list: ResultList = [];
 
-            if (_taxFeeItem.taxItem.rateType === RateType.PERCENT) {
-                return {
-                    key: `taxFee`,
-                    label: `${name} (%${formatNumber(_taxFeeItem.taxItem.rate)})`,
-                    value: formatMoney(_taxFeeItem.amount, "TRY")
-                };
-            }
-            return {
-                key: `taxFee`,
+    // 😊
+    list.push({
+        key: "taxFree",
+        label: "Vergisiz tutar",
+        value: formatMoney(prices.taxFree, "TRY")
+    });
+
+    // 😐
+    for (const [_index, _taxFeeItem] of taxFees.items.entries()) {
+        const name = `${_taxFeeItem.taxItem.name || `${_index + 1}. vergi`}`;
+
+        if (_taxFeeItem.taxItem.rateType === RateType.PERCENT) {
+            list.push({
+                key: `taxFee-${_index}`,
+                label: `${name} (%${normalizePrice(_taxFeeItem.taxItem.rate)})`, // FIXME: `rate` is not a price, so using `normalizePrice` feels misleading
+                value: formatMoney(_taxFeeItem.amount, "TRY")
+            });
+        } else if (_taxFeeItem.taxItem.rateType === RateType.UNIT) {
+            list.push({
+                key: `taxFee-${_index}`,
                 label: `${name} (${formatMoney(_taxFeeItem.taxItem.rate, _taxFeeItem.taxItem.rateTypeUnitCurrency)})`,
                 value: formatMoney(_taxFeeItem.amount, "TRY")
-            };
-        }),
-        {
-            key: "total",
-            label: `Toplam vergi (%${taxRates.total})`,
-            value: formatMoney(taxFees.total, "TRY")
-        },
-        {
-            key: "taxAdded",
-            label: "Tahmini satış fiyatı",
-            value: formatMoney(prices.taxAdded, "TRY")
+            });
         }
-    ];
+    }
+
+    // 😔
+    list.push({
+        key: "total",
+        label: `Toplam vergi (%${taxRates.total})`,
+        value: formatMoney(taxFees.total, "TRY")
+    });
+    list.push({
+        key: "taxAdded",
+        label: "Vergili tutar",
+        value: formatMoney(prices.taxAdded, "TRY")
+    });
+
+    return list;
 };
