d7e76e6dea809b20afef652c1407acd1c41bd8bc
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2025-11-06 20:27:00 +0300

    feat: add withVersion param to staticSiteUrl util

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -15,9 +15,12 @@ export const staticUrl = (path: UrlPath): URL => {
     return new URL(`${BASE_URL}${path}?v=${PACKAGE_JSON_VERSION}`);
 };
 
-export const staticSiteUrl = (path: UrlPath): URL => {
+export const staticSiteUrl = (path: UrlPath, withVersion = true): URL => {
     const url = siteUrl(path).href.replace(/\/$/, "");
-    return new URL(`${url}?v=${PACKAGE_JSON_VERSION}`);
+    if (withVersion) {
+        return new URL(`${url}?v=${PACKAGE_JSON_VERSION}`);
+    }
+    return new URL(url);
 };
 
 export const isCurrentPage = (currentUrl: URL, pageUrl: URL): boolean => {
