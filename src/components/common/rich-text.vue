73f68c9331d36ae721eac96837a76db19662b668
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2025-11-13 07:20:50 +0300

    design: stylize details tag in rich-text

--- src/components/common/rich-text.vue
+++ src/components/common/rich-text.vue
@@ -174,5 +174,99 @@ const props = withDefaults(defineProps<Props>(), {
         border-inline-start: calc(var(--vh-spacer) * .25) solid var(--vh-clr-primary);
         background: hsla(var(--vh-clr-white-hsl), .12)
     }
+
+
+    // Details
+    :deep(details) {
+        --_padding-inline: calc(var(--vh-spacer) * 1.25);
+        border: var(--vh-border-inline-size) solid hsla(var(--vh-clr-border-hsl), var(--vh-clr-border-alpha));
+        overflow: hidden;
+        margin-inline: calc(var(--container-padding-inline) * -1);
+        @include vh-media-breakpoint-down(sm) {
+            border-inline: 0
+        }
+        @include vh-media-breakpoint-up(sm) {
+            @include vh-squircle(var(--vh-br-normal));
+            margin-inline: calc(var(--_padding-inline) * -1)
+        }
+        & + details {
+            margin-block-start: calc(var(--vh-spacer) * .75)
+        }
+        &[open] {
+            background: hsla(var(--vh-clr-white-hsl), .08);
+            @supports (interpolate-size: allow-keywords) {
+                &::details-content {
+                    block-size: auto
+                }
+            }
+            summary {
+                &::after {
+                    transform: rotate(45deg) scale(1.5)
+                }
+                &::before {
+                    position: absolute;
+                    inset-block-end: 0;
+                    border-block-end: var(--vh-border-inline-size) dashed hsla(var(--vh-clr-border-hsl), var(--vh-clr-border-alpha));
+                    content: "";
+                    transition: vh-transition(opacity, var(--vh-duration-short));
+                    @include vh-media-breakpoint-down(sm) {
+                        inset-inline: var(--container-padding-inline)
+                    }
+                    @include vh-media-breakpoint-up(sm) {
+                        inset-inline: var(--_padding-inline)
+                    }
+                }
+            }
+        }
+        summary {
+            --_color-hsl: var(--color-hsl, var(--vh-clr-white-hsl));
+            --_bg-alpha: var(--bg-alpha, 0);
+            position: relative;
+            cursor: pointer;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+            gap: var(--vh-spacer);
+            font-weight: var(--vh-fw-bold);
+            background-color: hsla(var(--_color-hsl), var(--_bg-alpha));
+            padding: var(--vh-spacer) var(--container-padding-inline);
+            @include vh-media-breakpoint-up(sm) {
+                padding: var(--vh-spacer) calc(var(--vh-spacer) * 1.25)
+            }
+            @include vh-hover {
+                transition: vh-transition(background-color, var(--vh-duration-short));
+                &:hover {
+                    --bg-alpha: .08;
+                    &::before {
+                        opacity: 0
+                    }
+                }
+                &:active {
+                    --bg-alpha: .12
+                }
+            }
+            &::after {
+                inline-size: .75rem;
+                aspect-ratio: 1;
+                display: inline-block;
+                content: "";
+                transform-origin: center;
+                background: conic-gradient(from 90deg at 1px 1px, transparent 90deg, currentColor 0) calc(100% + .5px) calc(100% + .5px) / calc(50% + 1px) calc(50% + 1px);
+                transition: vh-transition(transform, var(--vh-duration-long), var(--vh-timing-spring))
+            }
+        }
+        & > div {
+            padding: var(--vh-spacer) calc(var(--vh-spacer) * 1.25)
+        }
+        @supports (interpolate-size: allow-keywords) {
+            &::details-content {
+                transition: vh-transition(block-size content-visibility, var(--vh-duration-long));
+                transition-behavior: allow-discrete;
+                block-size: 0;
+                overflow: clip
+            }
+        }
+    }
 }
 </style>
