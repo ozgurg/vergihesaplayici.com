969a76d1a508d8791d7027b6d841940757cf5475
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2025-12-29 00:00:59 +0300

    refactor: calculator tests

--- __test__/domains/kdv-hesaplayici/calculator.test.ts
+++ __test__/domains/kdv-hesaplayici/calculator.test.ts
@@ -8,110 +8,105 @@ type CalculateParams = {
 };
 
 const calculate = ({ input, expectedOutput }: CalculateParams): void => {
-    const calculator = new Calculator(input);
-    const results = calculator.calculate();
-    expect(results.prices).toStrictEqual(expectedOutput);
+    it(`input: ${JSON.stringify(input)}, expectedOutput: ${JSON.stringify(expectedOutput)}`, () => {
+        const calculator = new Calculator(input);
+        const results = calculator.calculate();
+        expect(results.prices).toStrictEqual(expectedOutput);
+    });
 };
 
 describe("domains/kdv-hesaplayici/calculator.js", () => {
-    it("calculates taxes correctly with given inputs", () => {
-        // "tax-free-to-tax-added"
-        calculate({
-            input: {
-                mode: "tax-free-to-tax-added",
-                price: 100,
-                rate: 15
-            },
-            expectedOutput: {
-                taxFree: 100,
-                taxAdded: 115
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-free-to-tax-added",
-                price: 250,
-                rate: 18
-            }, expectedOutput: {
-                taxFree: 250,
-                taxAdded: 295
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-free-to-tax-added",
-                price: 448,
-                rate: 25
-            }, expectedOutput: {
-                taxFree: 448,
-                taxAdded: 560
-            }
-        });
+    calculate({
+        input: {
+            mode: "tax-free-to-tax-added",
+            price: 100,
+            rate: 15
+        },
+        expectedOutput: {
+            taxFree: 100,
+            taxAdded: 115
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-free-to-tax-added",
+            price: 250,
+            rate: 18
+        }, expectedOutput: {
+            taxFree: 250,
+            taxAdded: 295
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-free-to-tax-added",
+            price: 448,
+            rate: 25
+        }, expectedOutput: {
+            taxFree: 448,
+            taxAdded: 560
+        }
+    });
 
+    calculate({
+        input: {
+            mode: "tax-added-to-tax-free",
+            price: 100,
+            rate: 15
+        }, expectedOutput: {
+            taxFree: 86.96,
+            taxAdded: 100
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-added-to-tax-free",
+            price: 250,
+            rate: 18
+        }, expectedOutput: {
+            taxFree: 211.86,
+            taxAdded: 250
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-added-to-tax-free",
+            price: 448,
+            rate: 25
+        }, expectedOutput: {
+            taxFree: 358.4,
+            taxAdded: 448
+        }
+    });
 
-        // "tax-added-to-tax-free"
-        calculate({
-            input: {
-                mode: "tax-added-to-tax-free",
-                price: 100,
-                rate: 15
-            }, expectedOutput: {
-                taxFree: 86.96,
-                taxAdded: 100
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-added-to-tax-free",
-                price: 250,
-                rate: 18
-            }, expectedOutput: {
-                taxFree: 211.86,
-                taxAdded: 250
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-added-to-tax-free",
-                price: 448,
-                rate: 25
-            }, expectedOutput: {
-                taxFree: 358.4,
-                taxAdded: 448
-            }
-        });
-
-
-        // "tax-to-tax-base"
-        calculate({
-            input: {
-                mode: "tax-to-tax-base",
-                price: 100,
-                rate: 15
-            }, expectedOutput: {
-                taxFree: 666.67,
-                taxAdded: 766.67
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-to-tax-base",
-                price: 250,
-                rate: 18
-            }, expectedOutput: {
-                taxFree: 1_388.89,
-                taxAdded: 1_638.89
-            }
-        });
-        calculate({
-            input: {
-                mode: "tax-to-tax-base",
-                price: 448,
-                rate: 25
-            }, expectedOutput: {
-                taxFree: 1_792,
-                taxAdded: 2_240
-            }
-        });
+    calculate({
+        input: {
+            mode: "tax-to-tax-base",
+            price: 100,
+            rate: 15
+        }, expectedOutput: {
+            taxFree: 666.67,
+            taxAdded: 766.67
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-to-tax-base",
+            price: 250,
+            rate: 18
+        }, expectedOutput: {
+            taxFree: 1_388.89,
+            taxAdded: 1_638.89
+        }
+    });
+    calculate({
+        input: {
+            mode: "tax-to-tax-base",
+            price: 448,
+            rate: 25
+        }, expectedOutput: {
+            taxFree: 1_792,
+            taxAdded: 2_240
+        }
     });
 });
