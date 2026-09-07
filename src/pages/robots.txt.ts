8753b218130863b61d841f022b0f9c6ebdd11a83
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2026-09-07 22:15:11 +0300

    fix: update robots.txt to allow all paths

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -3,11 +3,7 @@ import type { APIRoute } from "astro";
 export const GET: APIRoute = (): Response => {
     const body = [
         "user-agent: *",
-        "",
-        "disallow: /*?",
-        "disallow: /public/",
-        "disallow: /_astro/",
-        "disallow: /*.html$",
+        "allow: /",
         "",
         `sitemap: ${staticSiteUrl("/sitemap-index.xml", false)}`
     ].join("\n");
